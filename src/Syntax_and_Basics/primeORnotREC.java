54f75f92111403ceef8daa37122ca632155635bc
SivaNagaRaju <tsnraju.edu@gmail.com>
2023-06-22 23:07:49 +0530

    Visual Changes to primeORnotREC()

--- src/Syntax_and_Basics/primeORnotREC.java
+++ src/Syntax_and_Basics/primeORnotREC.java
@@ -8,20 +8,17 @@ public class primeORnotREC {
         int p = ob.readInteger("Enter a Number");
 
         x = ob2.primeORnot(p);
-        System.out.println(x);
 
         if(x) System.out.println(p+" is a Prime Number...!");
         else System.out.println(p+" is Not A Prime Number...!");
     }
 
     boolean primeORnot(int p){
-        var ob = new display();
         if(p == 2) return true;
 
         else if(p > 2){
             int n,i,j,L[];
             n = (int) Math.ceil(Math.sqrt(p));
-            System.out.println("n = "+n);
             L = new int[n-1];
 
             j = 0;
@@ -31,11 +28,8 @@ public class primeORnotREC {
                     j++;
                 }
             }
-
-            ob.list(L, n-1);
-
+            
             for(i=0; i<j; i++){
-                System.out.println("p, L[i], p/L[i]: "+p+", "+L[i]+", "+p/L[i]);
                 if(p % L[i] == 0){
                     return false;
                 }
